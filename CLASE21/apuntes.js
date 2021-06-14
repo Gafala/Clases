b607ec8eb86e597e0a429703862fd6e65deb1f1e
gafala <gastonfablafuente@gmail.com>
2021-06-14 17:53:42 -0300

    instala express

---
+++ CLASE21/apuntes.js
@@ -0,0 +1,12 @@
+/**
+ * Servidor : Desde donde se mantiene la página o aplicación
+ * Cliente : Quien está del lado visual 
+ * Request : pedido al servidor desde el cliente
+ * Response : La respuesta del servior hacia el cliente
+ 
+
+EXPRESS : Framework de NODE.JS
+
+git init.
+*/
+
